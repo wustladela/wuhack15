10e52770c7ceef0d5ce7dc8b5041610cd8022502
Jason Li <jason.li@wustl.edu>
2015-09-20 09:31:14 -0500

    Create logout.js

---
+++ logout.js
@@ -0,0 +1,9 @@
+Parse.initialize("eamcKJmUTepWXqQzYx5iNmgVUcX55xvCQX749IfY", "Gg3ZOGnMTkHaMIOZ0OJHsig6QwU5j8Jev2RkIZML");
+
+document.getElementById("logoutButton").addEventListener("click", logout, false);
+
+function logout(event) {
+
+  Parse.User.logout();
+
+}
