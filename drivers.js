b1d18c64c47f7cb21bd9430874641ace8f453430
Jason Li <jason.li@wustl.edu>
2015-09-20 11:06:55 -0500

    Update drivers.js

--- drivers.js
+++ drivers.js
@@ -1,6 +1,7 @@
 Parse.initialize("eamcKJmUTepWXqQzYx5iNmgVUcX55xvCQX749IfY", "Gg3ZOGnMTkHaMIOZ0OJHsig6QwU5j8Jev2RkIZML");
 
 document.getElementById("driverSubmitBtn").addEventListener("click", submitRide, false);
+document.addEventListener("DOMContentLoaded", getExistingRides, false);
 
 function submitRide(event) {
 	var pickupLoc = document.getElementById("pickupLoc").value;
@@ -30,6 +31,7 @@ function submitRide(event) {
   rides.set("numSeats", numSeats);
   rides.set("price", price);
   rides.set("createdBy", Parse.User.current());
+  rides.set("riders", []);
   var first = Parse.User.current().get("firstName");
   var last = Parse.User.current().get("lastName");
   var driverName = first + " " + last;
@@ -46,4 +48,69 @@ function submitRide(event) {
    alert('Failed to create new object, with error code: ' + error.message);
 	}
 });
+}
+
+function getExistingRides(event) {
+  var user = Parse.User.current();
+  var userExistingRides = user.get("existingRides");
+  for (var i = 0; i < userExistingRides.length; i++) {
+    var UsersRides = Parse.Object.extend("Rides");
+    var usersRides = new UsersRides();
+    var ridesQuery = new Parse.Query(UsersRides);
+    ridesQuery.equalTo("objectId", userExistingRides[i]);
+    ridesQuery.find({
+      success: function(ride) {
+        var node = document.createElement("div");
+        node.class = "posting";
+
+        var destination = document.createElement("h3");
+        destination.innerHTML = ride[0].get("destination");
+        node.appendChild(destination);
+
+        var nameLabel = document.createElement("label");
+        nameLabel.innerHTML = "Driver's Name: ";
+        node.appendChild(nameLabel);
+        var driversName = document.createTextNode(ride[0].get("driverName"));
+        node.appendChild(driversName);
+        var br1 = document.createElement("br"); 
+        node.appendChild(br1);
+
+        var pickupLabel = document.createElement("label");
+        pickupLabel.innerHTML = "Pickup Location:    ";
+        node.appendChild(pickupLabel);
+        var pickupLoc = document.createTextNode(ride[0].get("pickupLoc"));         
+        node.appendChild(pickupLoc);
+        var br3 = document.createElement("br"); 
+        node.appendChild(br3);
+
+        var dateLabel = document.createElement("label");
+        dateLabel.innerHTML = "Date:    ";
+        node.appendChild(dateLabel);
+        var date = new Date(ride[0].get("date"));
+        var localDate = date.toLocaleTimeString();
+        var dateNode = document.createTextNode(date);   
+        node.appendChild(dateNode);    
+        var br4 = document.createElement("br"); 
+        node.appendChild(br4);
+
+        var priceLabel = document.createElement("label");
+        priceLabel.innerHTML = "Price:    ";
+        node.appendChild(priceLabel);
+        var price = document.createTextNode(ride[0].get("price"));
+        node.appendChild(price);
+        var br5 = document.createElement("br"); 
+        node.appendChild(br5);
+
+        var availabilityLabel = document.createElement("label");
+        availabilityLabel.innerHTML = "Availability:    ";
+        node.appendChild(availabilityLabel);
+        var availability = document.createTextNode(ride[0].get("numSeats"));
+        node.appendChild(availability);
+        var br6 = document.createElement("br"); 
+        node.appendChild(br6);
+
+        document.getElementById("existingRides").appendChild(node);
+      }
+    });
+  }
 }
